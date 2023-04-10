6b4ee5b249c95a47e7ae1fbba320e6475ebbd84f
mikha <Mikhailov.dzenphone@gmail.com>
2023-04-10 15:58:57 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int money = 15000;
+        double percent = 0.01;
+        int targetMoney = 2459000;
+        int total = 0;
+        int month = 1;
+
+        while (total<targetMoney) {
+            total += money + total * percent;
+            System.out.println("Месяц " + month + ", количество накоплений " + total);
+            month++;
+        }
     }
 }
